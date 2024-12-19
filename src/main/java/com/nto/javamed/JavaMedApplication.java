f47c41dc7a3f3731285c2e7bfdbcaab683c64b87
Antoine Marechal-Balle <anto.marechal@icloud.com>
2024-12-19 09:10:59 +0100

    Initial commit

---
+++ src/main/java/com/nto/javamed/JavaMedApplication.java
@@ -0,0 +1,13 @@
+package com.nto.javamed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaMedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nto/javamed/JavaMedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nto.javamed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
